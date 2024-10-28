30d8adbb750c960a433fcac342b7c3136df5ecff
DIR\marisa.tarantino <marisa.tarantino@accenture.com>
2024-10-28 17:50:32 +0100

    Inizio esercizio con get e set

--- src/app/lista/lista.component.ts
+++ src/app/lista/lista.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { FormControl } from '@angular/forms';
 import { ActivatedRoute, Router } from '@angular/router';
+import { SharedService } from '../shared/services/shared.service';
 
 interface Food {
   value: string;
@@ -21,7 +22,7 @@ export class ListaComponent {
   showCibo: boolean = false;
   showTest: boolean = false;
 
-  constructor(private route: ActivatedRoute, private router: Router) {
+  constructor(private route: ActivatedRoute, private router: Router, private sharedService: SharedService) {
     this.selectedValue = this.listaCibi[0].viewValue;
   }
 
@@ -50,8 +51,9 @@ export class ListaComponent {
     this.listaCibi.splice(this.listaCibi.length - 1, 1);
   }
 
-  mostraRicetta(cibo: Food) {
-    this.router.navigateByUrl('ricetta/' + this.cibiFC.value);
+  mostraRicetta() {
+    this.sharedService.setAttUtilObj('cibi', {cibo: this.cibiFC.value});
+    this.router.navigateByUrl('ricetta');
   }
 
   mostraCibo(cibo: string) {
--- src/app/ricetta/ricetta.component.ts
+++ src/app/ricetta/ricetta.component.ts
@@ -1,6 +1,7 @@
 import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
 import { FormControl, Validators } from '@angular/forms';
 import { ActivatedRoute } from '@angular/router';
+import { SharedService } from '../shared/services/shared.service';
 
 @Component({
   selector: 'app-ricetta',
@@ -8,17 +9,20 @@ import { ActivatedRoute } from '@angular/router';
   styleUrl: './ricetta.component.scss'
 })
 export class RicettaComponent implements OnInit {
+  
 
 
-  constructor(private route: ActivatedRoute) { }
+  constructor(private route: ActivatedRoute, private sharedService: SharedService) { }
 
   ricettaFC = new FormControl('');
   pageTitle?: string | null;
+  
 
   @Output() mandaRicetta: EventEmitter<string> = new EventEmitter();
 
   ngOnInit(): void {
-    this.pageTitle = this.route.snapshot.paramMap.get('value');
+    const cibiValore = this.sharedService.getAttUtilObj('cibi')
+    this.pageTitle = cibiValore;
   }
 
   onChangeRicetta(evento: Event) {
